01ecaa68a79a74bd559750291509995787544987
Juste Raimbault <juste.raimbault@polytechnique.edu>
2022-10-14 14:50:16 +0100

    par_pre_proc_data

--- Models/MethodsBenchmark/preproc.R
+++ Models/MethodsBenchmark/preproc.R
@@ -5,10 +5,20 @@ setwd(paste0(Sys.getenv("CS_HOME"),'/NetworksTerritories/HistoricalData/'))
 
 library(jsonlite)
 library(sf)
+library(parallel)
+library(doParallel)
+library(foreach)
 
-datadir = paste0(Sys.getenv("CS_HOME_EXT"),'/NetworksTerritories/HistoricalData/Data/directories_geocoded/')
+# local
+#datadir = paste0(Sys.getenv("CS_HOME_EXT"),'/NetworksTerritories/HistoricalData/Data/directories_geocoded/')
+# remote 
+datadir = paste0(Sys.getenv("CS_HOME"),'/NetworksTerritories/HistoricalData/Data/directories_geocoded/')
+
 
 files = list.files(datadir, pattern="DidotBottin")
+
+cl = parallel::makeCluster(parallel::detectCores())
+doParallel::registerDoParallel(cl)
 
 # columns to export: lon, lat, activity, year (no need of adress)
 # rq: for coarse, should compute spatial sd from bbox of street geom? -> later if test on all (for now only precise)
@@ -17,16 +27,22 @@ files = list.files(datadir, pattern="DidotBottin")
 # do this here
 todel = c("[","]",".","\\n",",","\"",";","-",":","(",")","\n")
 
-start=as.numeric(Sys.time())
-alldata=data.frame()
-for(i in 1:length(files)){
+#start=as.numeric(Sys.time())
+#alldata=data.frame()
+#for(i in ){
+# test
+#alldata <- foreach(i =1:40,.combine=rbind) %dopar% {
+alldata <- foreach(i =1:length(files),.combine=rbind) %dopar% {
+  library(jsonlite)
+  library(sf)
   #show(i)
-  if(i%%10==0){
-    show(paste0(i," / ",length(files), " in ",(as.numeric(Sys.time())-start)/60," min"))
-    write.table(alldata,file=paste0(datadir,'processed/',i,'.csv'),row.names = F,col.names = F,sep=',')
-    alldata=data.frame()
-    gc()
-  }
+  # local run
+  #if(i%%10==0){
+  #  show(paste0(i," / ",length(files), " in ",(as.numeric(Sys.time())-start)/60," min"))
+  #  write.table(alldata,file=paste0(datadir,'processed/',i,'.csv'),row.names = F,col.names = F,sep=',')
+  #  alldata=data.frame()
+  #  gc()
+  #}
   f = files[i]
   currentyear = substr(strsplit(f,'_')[[1]][2],1,4)
   currentraw = read_json(paste0(datadir,f))
@@ -56,8 +72,11 @@ for(i in 1:length(files)){
   currentdata = t(currentdata)
   currentdata=as.data.frame(cbind(currentdata,rep(currentyear,nrow(currentdata))))
   names(currentdata)<-c("activity","lon","lat","precision","year")
-  alldata = rbind(alldata,currentdata)
+  #alldata = rbind(alldata,currentdata)
+  return(currentdata)
 }
 
-write.table(alldata,file=paste0(datadir,'processed/',i,'.csv'),row.names = F,col.names = F,sep=",")
+write.table(alldata,file=paste0(datadir,'processed/alldata_didotbottin.csv'),row.names = F,col.names = T,sep=",")
+
+parallel::stopCluster(cl = cl)
 
